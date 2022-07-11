9d40199cad9ed704b6feb17babf14a7bc426e383
purzbeats <97489706+purzbeats@users.noreply.github.com>
2022-07-10 20:15:51 -0400

    Updates

--- sketch.js
+++ sketch.js
@@ -1,31 +1,20 @@
-var blockWidth = fxrand() * 35 + 5;
-var blockHeight = fxrand() * 35 + 5;
+let x = 0;
+let y = 0;
+let spacing;
+let speed;
+
+var cnv;
+
+var blockWidth = 96;
+var blockHeight = 64;
+
+var drawSpeed = 1;
+
+var scribble = new Scribble();  
 
 let data;
 let index = 0;
 
-var speedX = fxrand();
-var speedY = fxrand();
-
-var speedMult = 15;
-var speedFloor = 2;
-
-var speedMult1 = fxrand() * speedMult + speedFloor;
-var speedMult2 = fxrand() * speedMult + speedFloor;
-var speedMult3 = fxrand() * speedMult + speedFloor;
-var speedMult4 = fxrand() * speedMult + speedFloor;
-var speedMult5 = fxrand() * speedMult + speedFloor;
-var speedMult6 = fxrand() * speedMult + speedFloor;
-var speedMult7 = fxrand() * speedMult + speedFloor;
-var speedMult8 = fxrand() * speedMult + speedFloor;
-var speedMult9 = fxrand() * speedMult + speedFloor;
-var speedMult10 = fxrand() * speedMult + speedFloor;
-var speedMult11 = fxrand() * speedMult + speedFloor;
-var speedMult12 = fxrand() * speedMult + speedFloor;
-var speedMult13 = fxrand() * speedMult + speedFloor;
-var speedMult14 = fxrand() * speedMult + speedFloor;
-var speedMult15 = fxrand() * speedMult + speedFloor;
-var speedMult16 = fxrand() * speedMult + speedFloor;
 
 function preload() {
   data = loadJSON("palettes.json");
@@ -57,12 +46,58 @@ function choices() {
   col3 = palette[3];
   col4 = palette[4];
   paletteName = str(palette[5]);
+
+  speedMult = 1;
+  speedFloor = 1;
+
+  spacing = Math.floor(fxrand() * 4 + 2);
+  speed = 3;
+
+  speedX = 1;
+  speedY = 1;
+
+  blockSizeMult1 = fxrand() * 64 + 24;
+  blockSizeMult2 = fxrand() * 64 + 24;
+  blockSizeMult3 = fxrand() * 64 + 24;
+  blockSizeMult4 = fxrand() * 64 + 24;  
+
+  speedMult1 = fxrand() * speedMult + speedFloor;
+  speedMult2 = fxrand() * speedMult + speedFloor;
+  speedMult3 = fxrand() * speedMult + speedFloor;
+  speedMult4 = fxrand() * speedMult + speedFloor;
+  speedMult5 = fxrand() * speedMult + speedFloor;
+  speedMult6 = fxrand() * speedMult + speedFloor;
+  speedMult7 = fxrand() * speedMult + speedFloor;
+  speedMult8 = fxrand() * speedMult + speedFloor;
+  speedMult9 = fxrand() * speedMult + speedFloor;
+  speedMult10 = fxrand() * speedMult + speedFloor;
+  speedMult11 = fxrand() * speedMult + speedFloor;
+  speedMult12 = fxrand() * speedMult + speedFloor;
+  speedMult13 = fxrand() * speedMult + speedFloor;
+  speedMult14 = fxrand() * speedMult + speedFloor;
+  speedMult15 = fxrand() * speedMult + speedFloor;
+  speedMult16 = fxrand() * speedMult + speedFloor;
+
+}
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+}
+
+
+function windowResized() {
+  centerCanvas();
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  frameRate(60);
-  
+ 
+  cnv = createCanvas(800, 800);
+  centerCanvas();
+
+  pixelDensity(2);
+
   choices();
   let c = color(bg);
   background(c);
@@ -74,60 +109,78 @@ function setup() {
 
 function drawText() {
       fill(0);
-      rect(10, windowHeight - 100, 790, 90);
+      rect(0, height - 150, 800, 150);
       textFont('Courier');
       textSize(25);
       fill(255);
-      text(fxhash, 20, windowHeight - 50, windowWidth, windowHeight);
-      text("Palette: " + paletteName, 20, windowHeight - 85, windowWidth, windowHeight);
+      text(fxhash, 20, height - 50, width, height);
+      text("Palette: " + paletteName, 20, height - 85, width, height);
+      text("Frame: " + frameCount, 20, height - 120, width, height);
 }
+
+function getScribbleRect() {
+
+      blockMult = 1;
+
+      for (let z = 0; z < 1; z++) {
+       stroke(col1);
+       strokeWeight(fxrand() * 3);
+       scribble.scribbleRect((z + frameCount * speedX * speedMult1) % width, (z + frameCount * speedY * speedMult4) % height, blockWidth, blockHeight);
+
+       stroke(col2);
+       strokeWeight(fxrand() * 3);
+       scribble.scribbleRect((z + frameCount * speedX * speedMult2) % width, (z + frameCount * speedY * speedMult3) % height, blockWidth, blockHeight);
+
+       stroke(col3);
+       strokeWeight(fxrand() * 3);
+       scribble.scribbleRect((z + frameCount * speedX * speedMult3) % width, (z + frameCount * speedY * speedMult2) % height, blockWidth, blockHeight);
+
+       stroke(col4);
+       strokeWeight(fxrand() * 3);
+       scribble.scribbleRect((z + frameCount * speedX * speedMult4) % width, (z + frameCount * speedY * speedMult1) % height, blockWidth, blockHeight);
+      } 
+
+}
+
+function getRect() {
+       stroke(0);
+       strokeWeight(1);
+       rectMode(CENTER);       
+       
+       fill(col1);
+       rect(x, 100, blockWidth / 8, blockHeight * fxrand() + 24); 
+       fill(col2);
+       rect(x, 200, blockWidth / 16, blockHeight * fxrand() + 2); 
+       fill(col3);
+       rect(x, 300, blockWidth / 8, blockHeight * fxrand() + 24); 
+       fill(col4);
+       rect(x, 400, blockWidth / 4, blockHeight * fxrand() + 2); 
+       fill(col1);
+       rect(x, 500, blockWidth / 8, blockHeight * fxrand() + 24); 
+       fill(col2);
+       rect(x, 600, blockWidth / 16, blockHeight * fxrand() + 2); 
+       fill(col3);
+       rect(x, 700, blockWidth / 8, blockHeight * fxrand() + 24); 
+       
+       
+       if (x < width + 100) {
+          x = x + spacing; 
+       }
+       
+} 
+
+
+
 
 function draw() {
 
-      for (let z = 0; z < 1; z++) {
-       let c = color(col1);
-       fill(c);
-       stroke(0);
-       strokeWeight(1);
-       rect((z + frameCount * speedX * speedMult1) % width, (z + frameCount * speedY * speedMult4) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult2) % width, (z + frameCount * speedY * speedMult3) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult3) % width, (z + frameCount * speedY * speedMult2) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult4) % width, (z + frameCount * speedY * speedMult1) % height, blockWidth, blockHeight);
-      } 
 
-      for (let z = 0; z < 1; z++) {
-       let c = color(col2);
-       fill(c);
-       stroke(0);
-       strokeWeight(1);
-       rect((z + frameCount * speedX * speedMult5) % width, (z + frameCount * speedY * speedMult8) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult6) % width, (z + frameCount * speedY * speedMult7) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult7) % width, (z + frameCount * speedY * speedMult6) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult8) % width, (z + frameCount * speedY * speedMult5) % height, blockWidth, blockHeight);
-      }
 
-      for (let z = 0; z < 1; z++) {
-       let c = color(col3);
-       fill(c);
-       stroke(0);
-       strokeWeight(1);
-       rect((z + frameCount * speedX * speedMult9) % width, (z + frameCount * speedY * speedMult12) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult10) % width, (z + frameCount * speedY * speedMult11) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult11) % width, (z + frameCount * speedY * speedMult10) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult12) % width, (z + frameCount * speedY * speedMult9) % height, blockWidth, blockHeight);
-      } 
 
-       for (let z = 0; z < 1; z++) {
-       let c = color(col4);
-       fill(c);
-       stroke(0);
-       strokeWeight(1);
-       rect((z + frameCount * speedX * speedMult13) % width, (z + frameCount * speedY * speedMult16) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult14) % width, (z + frameCount * speedY * speedMult15) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult15) % width, (z + frameCount * speedY * speedMult14) % height, blockWidth, blockHeight);
-       rect((z + frameCount * speedX * speedMult16) % width, (z + frameCount * speedY * speedMult13) % height, blockWidth, blockHeight);
-      } 
-
+      getRect();
+      
+      // getScribbleRect();
+      // speedMult = speedMult + 1000;
 
 }
 
